6efdc64c46c5b9ab21bbad5d96918f76311346aa
Anastasia Silqa Lapteva <stasya.mango@gmail.com>
2023-03-16 00:55:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 130;
+        int payment = 1800;
+        int bonus = 0;
+        int grade = 100;
+
+        if (payment > 1000) {;
+            bonus = payment / grade;
+        }
+
+        balance = balance + payment + bonus;
+
+        System.out.println("Bonus: " + bonus);
+        System.out.println("Balance: " + balance);
+    }
+}
